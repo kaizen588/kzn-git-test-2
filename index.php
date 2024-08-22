99e4bcb627fc73871c975ef090e4d5c360de995b
kaizen588 <welly.business31@gmail.com>
2024-08-22 20:19:52 +0700

    Update index from branch mainbranch.php

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coba Git Remote</title>
+    <title>Ubah tittle ini dari branch main</title>
 </head>
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sint iure, dolorem nihil hic doloribus in eum necessitatibus et omnis culpa nemo soluta dignissimos unde odio aut eaque, dolores animi!</p>
     <ul>
-        <li>baris pertama
-        </li>
+        <li>baris pertama</li>
+        <li>baris kedua dari branch main hehe</li>
     </ul>
 </body>
 </html>
