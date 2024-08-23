405bad2e7defe6b21d7c18929387946746a3ab49
Welly <welly.business31@gmail.com>
2024-08-23 19:54:05 +0700

    update file index

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit tittle dari branch dummy-a</title>
+    <title>Edit tittle dari branch dummy-a tambah hehe</title>
 </head>
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sint iure, dolorem nihil hic doloribus in eum necessitatibus et omnis culpa nemo soluta dignissimos unde odio aut eaque, dolores animi!</p>
     <ul>
-        <li>baris pertama</li>
         <li>baris kedua dari branch main</li></li>
         <li>baris ketiga dari branch main</li>
+        <li>baris keempat dari branch 'main'</li>
     </ul>
 </body>
 </html>
